1cded37dbffd48243f7298a7b2263da419ac6c6d
Renuka Prasad <renukaprasad.kb@gmail.com>
2021-11-11 11:16:37 +0530

    landing page first push

---
+++ tailwindcss/tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    theme: {
+      screens: {
+        'sm':'600px',
+        'md':'768px',
+        '950px':'950px',
+        'lg':'1024px',
+        'xl':'1280px',
+        '2xl':'1536px'
+      },
+      extend: {
+       spacing: {
+         '72': '18rem',
+         '84': '21rem',
+         '96': '24rem',
+         'intro':'340px'
+       }
+      }
+    }
+  }
